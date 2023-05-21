b5fc5c406ab68aab524a6b032171d2f90a489c11
belovedpearl <>
2023-05-21 21:07:34 +0000

    Add the welcome text function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pyfiglet
+
+def welcome_text():
+    """
+     Displays the title of the game
+    """
+    print(pyfiglet.figlet_format("LINE MATCH", font="starwars", justify="center", width=110))
+
+
+
+welcome_text()
