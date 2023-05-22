2fc8aeb9f810a11de24e211b7282fe5a6b4c71a5
belovedpearl <>
2023-05-22 04:02:10 +0000

    Define the path to access the game instruction and open up the new game

--- run.py
+++ run.py
@@ -4,24 +4,54 @@
 import pyfiglet
 
 name = ""   # Global variable declared to be changed with each user
+def instruct():
+    print("To start the game, pick your desired starting points.\n")
+    print("Computer randomly creates a 5 x 5 grid of alphabets, you are to guess what row is/are likely to contain the same set of alphabet\n")
+    print("To guess, choose between 1-5.\n")
+    print("Commit some points to each lines.\n")
+    print("Press enter to play the game till you have no more or less points to continue.\n")
+
+
+def new_game(name):
+    """
+       Opens up the instruction and the game section
+    """
+    print(f"Welcome {name}, choose from the option below...\n")
+    decision = input("Press i to view instruction. \nPress n to play new game\n")
+    if decision == "i":
+        instruct()
+    elif decision == "n":
+        open_game(name)
+    else:
+        print("You have not chosen a valid option.\n")
 
 def welcome_text():
     """
-     Displays the title of the game
+      Displays the title of the game
     """
     print(pyfiglet.figlet_format("LINE MATCH", font="starwars", justify="center", width=110))
 
 
-
 def main():
+    """
+      Defines the main function
+    """
     welcome_text()
     print()
     print()
     print("Welcome to the game...")
     global name
     name = input("Enter your name: ")
-    
-
+    while True:
+        new_game_decision = input('Press y to start the game\n or \n Press n to quit:\n')
+        if new_game_decision == 'y':
+            new_game(name)
+            break
+        elif new_game_decision == 'n':
+            main()
+            break
+        else:
+            print('Please enter either y or n')
 
 main()
 print(name)
